4be88464c23a7512330c4588d1b6fded14cc42b1
Jeff Quinn <ns8riders@me.com>
2017-10-27 16:33:05 -0600

    update to images and padding

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,20 +5,6 @@ import s from './Gallery.css';
 
 const PHOTO_SET = [
   {
-    src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/showerSteam.jpg',
-    margin: '4',
-    srcset: [
-      'https://s3-us-west-2.amazonaws.com/tnr-pics/showerCustom1.JPG 1024w',
-      'https://s3-us-west-2.amazonaws.com/tnr-pics/showerCustom1.JPG 800w',
-      'https://s3-us-west-2.amazonaws.com/tnr-pics/showerCustom1.JPG 500w',
-      'https://s3-us-west-2.amazonaws.com/tnr-pics/showerCustom1.JPG 320w',
-    ],
-    sizes: ['(min-width: 480px) 50vw', '(min-width: 1024px) 33.3vw', '100vw'],
-    width: 600,
-    height: 600,
-    alt: 'image 1',
-  },
-  {
     src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/showerUpDownDeco.JPG',
     srcset: [
       'http://example.co m/example/img2_1024.jpg 1024w',
@@ -45,19 +31,6 @@ const PHOTO_SET = [
     alt: 'image 3',
   },
   {
-    src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/splashGlass2.JPG',
-    srcset: [
-      'http://example.co m/example/img2_1024.jpg 1024w',
-      'http://example.com/example/img2_800.jpg 800w',
-      'http://example.com/example/img2_500.jpg 500w',
-      'http://example.com/example/img2_320.jpg 320w',
-    ],
-    sizes: ['(min-width: 480px) 50vw', '(min-width: 1024px) 33.3vw', '100vw'],
-    width: 600,
-    height: 600,
-    alt: 'image 4',
-  },
-  {
     src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/showerWhiteBrick.JPG',
     srcset: [
       'http://example.co m/example/img2_1024.jpg 1024w',
@@ -71,19 +44,6 @@ const PHOTO_SET = [
     alt: 'image 4',
   },
   {
-    src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/shower45.JPG',
-    srcset: [
-      'http://example.co m/example/img2_1024.jpg 1024w',
-      'http://example.com/example/img2_800.jpg 800w',
-      'http://example.com/example/img2_500.jpg 500w',
-      'http://example.com/example/img2_320.jpg 320w',
-    ],
-    sizes: ['(min-width: 480px) 50vw', '(min-width: 1024px) 33.3vw', '100vw'],
-    width: 600,
-    height: 600,
-    alt: 'Master Shower on a 45 degree angle',
-  },
-  {
     src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/entrySlate.JPG',
     srcset: [
       'http://example.co m/example/img2_1024.jpg 1024w',
@@ -95,19 +55,6 @@ const PHOTO_SET = [
     width: 600,
     height: 600,
     alt: 'Entry Slate Floor',
-  },
-  {
-    src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/floorHardwoodGray.JPG',
-    srcset: [
-      'http://example.co m/example/img2_1024.jpg 1024w',
-      'http://example.com/example/img2_800.jpg 800w',
-      'http://example.com/example/img2_500.jpg 500w',
-      'http://example.com/example/img2_320.jpg 320w',
-    ],
-    sizes: ['(min-width: 480px) 50vw', '(min-width: 1024px) 33.3vw', '100vw'],
-    width: 600,
-    height: 600,
-    alt: 'Floor tile that looks like hardwood',
   },
   {
     src: 'https://s3-us-west-2.amazonaws.com/tnr-pics/showerMasterWhite.JPG',
@@ -128,7 +75,7 @@ class ImgGallery extends React.Component {
   render() {
     return (
       <div className={s.container}>
-        <div className={s.caption}>Create the space you always wanted</div>
+        <div className={s.caption}>Create the space you have always wanted</div>
         <Gallery photos={PHOTO_SET} margin="4" columns={5} />
       </div>
     );
